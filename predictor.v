8d99676f656cc8213aa3ad2a7b55494034efeac8
amirkoochk <94669853+amirkoochk@users.noreply.github.com>
2023-06-01 10:50:28 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -6,19 +6,23 @@ module predictor(input wire request, result, clk, taken, output reg prediction);
   // Output prediction when request is 1
   always @(posedge clk) begin
     if (request == 1) begin
-      prediction <= saturation_counter < 2'd2 ? 1 : 0;
+      prediction <= saturation_counter >= 2'd2 ? 1 : 0;
     end
   end
   
   // Update saturation counter when result is 1 and taken is high
   always @(posedge clk) begin
-    if (result == 1 && taken) begin
-      if (saturation_counter < 2'd3) begin
-        saturation_counter <= saturation_counter + 1;
-      end
-    end else begin
-      saturation_counter <= saturation_counter > 0 ? saturation_counter - 1 : 0;
-    end
+    if (result == 1) 
+    begin
+      if (taken && saturation_counter < 2'd3) 
+        begin
+          saturation_counter <= saturation_counter + 1;
+        end
+    else if (saturation_counter != 2'd0 && !taken)
+        begin
+            saturation_counter <= saturation_counter - 1;
+        end
+    end 
   end
 
 endmodule
