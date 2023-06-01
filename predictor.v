46dd3db90f4e3e2a80506c9569b8a0890ca424b2
amirkoochk <94669853+amirkoochk@users.noreply.github.com>
2023-06-01 10:19:12 +0330

    first commit

--- predictor.v
+++ predictor.v
@@ -1,5 +1,20 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
 
-// Your code
+parameter WIDTH = 3; 
+reg [WIDTH-1:0] counter = 0;
 
+always @(posedge clk) begin
+    if (taken) begin
+        if (counter < {{WIDTH}}'d7) 
+            counter <= counter + {{WIDTH}}'d1; 
+    end else begin
+        if (counter > {{WIDTH}}'d0)
+            counter <= counter - {{WIDTH}}'d1; 
+    end
+    
+    if (counter > {{WIDTH}}'d3) 
+        prediction <= 1'b1;
+    else
+        prediction <= 1'b0;
+end
 endmodule
